22c26d3ce0e55db2313c4d13a180b649ed5f7352
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-07-16 23:46:27 +1000

    Create browser router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {App} from "./components";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Assessments, Configuration, Dashboard, Error, Subjects, WhatIf} from "./routes";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element: <Dashboard />
+            },
+            {
+                path: "/subjects",
+                element: <Subjects />
+            },
+            {
+                path: "/assessments",
+                element: <Assessments />
+            },
+            {
+                path: "/what-if",
+                element: <WhatIf />
+            },
+            {
+                path: "/configuration",
+                element: <Configuration />
+            }
+        ]
+    },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
